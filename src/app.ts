a43408d9365e5e9e4c4cabe4eeea79950418106f
rehadhasan <rehadhasan664@gmail.com>
2025-01-27 00:14:17 +0600

    crate successfully

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app: Application = express();
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
 app.use(bodyParser.json());
 
 // application route
