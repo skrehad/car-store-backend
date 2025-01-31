0093fd52defb514f750cec20a9371bffbe9f9f4c
rehadhasan <rehadhasan664@gmail.com>
2025-02-01 02:23:32 +0600

    cors update

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['https://assignment-4-frontend-psi.vercel.app/'],
+    origin: ['https://assignment-4-frontend-psi.vercel.app'],
     credentials: true,
   }),
 );
